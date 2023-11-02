9bf93c2dded73e24ccb7c4213707c19234e3c053
adityash12 <128187345+adityash12@users.noreply.github.com>
2023-11-03 02:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+//your JS code here. If required
+function f1(){
+let h1Tag= document.createElement("h1"); //we create new tag that is h1 tag
+let para=document.getElementById("status");
+h1Tag.innerText="Entered Metaverse"; //in this we add some text
+let parent= document.querySelector("#mainBody");
+parent.insertBefore(h1Tag,para);
+para.remove();
+}
+
+
+
