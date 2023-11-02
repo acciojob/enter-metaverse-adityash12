f74af6b6882b02a56befe4159e5f873883518009
adityash12 <128187345+adityash12@users.noreply.github.com>
2023-11-03 03:13:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required
 function f1(){
 let h1Tag= document.createElement("h1"); //we create new tag that is h1 tag
-let para=document.getElementById("status");
+let para=document.querySelector("#status");
 h1Tag.innerText="Entered Metaverse"; //in this we add some text
 let parent= document.querySelector("#mainBody");
 parent.insertBefore(h1Tag,para);
